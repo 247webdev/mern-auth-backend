01117d863adf8e1dbefd46aa2d287a2084223b4d
kbbushman <k.bushman@yahoo.com>
2018-05-15 09:52:53 -0700

    Adds routes for users, posts, and profile

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+
+// Routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 // Init App
 const app = express();
@@ -15,6 +20,11 @@ mongoose.connect(db)
 
 // Test Route
 app.get('/', (req, res) => res.send('Hello World...'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 // Start Server
 app.listen(port, () => console.log(`Servers running on port ${port}`));
